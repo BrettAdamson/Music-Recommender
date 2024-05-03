442f9294f7de6a6333d00be0614bf580af14c60c
BrettAdamson <brettmadamson@gmail.com>
2024-05-02 22:27:11 -0500

    added basic submit form for song and artist

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class SongForm(FlaskForm):
     song = StringField("Song", validators=[DataRequired()])
-    artist = PasswordField("Artist", validators=[DataRequired()])
+    artist = StringField("Artist", validators=[DataRequired()])
     submit = SubmitField("Submit")
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import SongForm
 
 
 @app.route("/")
@@ -7,3 +8,16 @@ from flask import render_template
 def index():
     user = {"username": "Brett"}
     return render_template("index.html", title="Home", user=user)
+
+
+@app.route("/search", methods=["GET", "POST"])
+def song_search():
+    form = SongForm()
+    if form.validate_on_submit():
+        print("valid")
+        flash("{} by {}".format(form.song.data, form.artist.data))
+        # return redirect("/index")
+    else:
+        print("invalid song")
+
+    return render_template("song_form.html", title="Song Search", form=form)

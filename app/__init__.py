ccae72a8e47ef0c0edbe22498eb8e23bb98ad742
BrettAdamson <brettmadamson@gmail.com>
2024-05-09 21:39:26 -0500

    refactoring where the api and url route code resides

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from config import Config
 
-from .api.routes import api
-from .website.routes import website
 
+def create_app(config_class=Config):
+    from .api.routes import api
+    from .website.routes import website
 
-app = Flask(__name__)
-app.config.from_object(Config)
-app.register_blueprint(api)
-app.register_blueprint(website)
-from app import routes
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.register_blueprint(api)
+    app.register_blueprint(website)
+    # from app import routes
+
+    return app
--- app/website/routes.py
+++ app/website/routes.py
@@ -4,6 +4,6 @@ from flask import Blueprint
 website = Blueprint("website", __name__)
 
 
-@website.route("/blueprint")
+@website.route("/")
 def web_index():
     return "<h1>Welcome to website blueprint</h1>"

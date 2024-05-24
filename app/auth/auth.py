56b12ede03057d603be119c67659f916bfef8578
BrettAdamson <49770729+BrettAdamson@users.noreply.github.com>
2024-05-23 21:06:59 -0500

    Feature/spotify auth api (#4)
    * function that hits spotify access token endpoint works
    
    * endpoint for spotify access bearer token is up
    
    * added test blueprints for better project structure
    
    * refactoring where the api and url route code resides
    
    * refactored spotify token to the api blueprint
    
    * refactored the application structure to use flask blueprints properly
    
    * trying to figure out how to structure the files that call Spotify
    
    * renamed website blueprint to core blueprint
    
    * api blueprint works and core blueprint can now import spotify_handler
    
    * got the basic oauth2 authentication flow down with redirect
    
    * fixed wrong grant_type for authorization code OAuth flow
    
    * working on storing oauth token and being able to retrieve it for Spotify endpoints
    
    * added basic token decorater
    
    * working on token required decorated function
    
    * working on decorators for when requests are made when the access token is expired or empty
    
    * hitting the spotify api now, just need to figure out the best way to refresh the access token using date math
    
    * error decorator for 401's on client credentials works
    
    * refresh token works individually, but when put together with the decorator it doesnt work
    
    * found auth redirect problem, will need to redirect to a 'login with spotify' page, if the user is not authorized
    
    * Basic redirect button works for when the user does not have an auth code

---
+++ app/auth/auth.py
@@ -0,0 +1,115 @@
+import os
+from dotenv import load_dotenv
+import requests
+from app.auth import bp
+from requests_oauthlib import OAuth2Session
+from requests.auth import HTTPBasicAuth
+from flask import request, session, redirect, render_template
+import json
+from functools import wraps
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+REDIRECT_URI = "http://127.0.0.1:5000/redirect"
+SCOPE = ["user-read-email", "playlist-read-collaborative"]
+BASE_AUTH_URL = "https://accounts.spotify.com/authorize"
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+
+
+@bp.route("/auth_code")
+def get_oauth_token():
+    spotify = OAuth2Session(CLIENT_ID, scope=SCOPE, redirect_uri=REDIRECT_URI)
+    auth_url, state = spotify.authorization_url(BASE_AUTH_URL)
+    print("url: " + auth_url)
+    print("state: " + state)
+    return redirect(auth_url)
+
+
+@bp.route("/redirect")
+def spotify_redirect():
+    print("In redirect")
+    code = request.args.get("code")
+    response = requests.post(
+        TOKEN_URL,
+        auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET),
+        data={
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": REDIRECT_URI,
+        },
+    )
+    auth_token = response.json()
+    session["auth_code"] = auth_token
+    return response.json()
+
+
+@bp.route("/refresh_token", methods=["GET"])
+def refresh_token():
+    print(session["auth_code"]["refresh_token"])
+    response = requests.post(
+        TOKEN_URL,
+        auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET),
+        data={
+            "grant_type": "refresh_token",
+            "refresh_token": session["auth_code"]["refresh_token"],
+        },
+    )
+    auth_token = response.json()
+
+    session["auth_code"] = auth_token
+    return response.json()
+
+
+def valid_auth_code(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        try:
+            if not session.get("auth_code"):
+                print("IN DECORATOR")
+                # if no auth token redirect to login page
+                return render_template("spotify_login.html", title="Authorize")
+            return f(*args, **kwargs)
+        except requests.exceptions.HTTPError as err:
+            print("\nEXCEPTION RAISED\n")
+            if err.response.status_code == 401:
+                print("Stale Token. Using Refresh Token")
+                refresh_token()
+            return f(*args, **kwargs)
+
+    return decorated_function
+
+
+@bp.route("/client_credentials", methods=["GET"])
+def get_client_credentials():
+    payload = {
+        "grant_type": "client_credentials",
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+    }
+    response = requests.post("https://accounts.spotify.com/api/token", data=payload)
+    access_token = response.json()
+    session["client_code"] = access_token
+    return response.json()
+
+
+def valid_client_credentials(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        try:
+            if not session.get("client_code"):
+                get_client_credentials()
+            return f(*args, **kwargs)
+        except requests.exceptions.HTTPError as err:
+            print("\nEXCEPTION RAISED\n")
+            if err.response.status_code == 401:
+                print("Stale Token. Getting New Client Credentials")
+                get_client_credentials()
+
+            return f(*args, **kwargs)
+
+    return decorated_function

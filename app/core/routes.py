ecce3799bfca9ac2040f6b54c94e911666c5baba
BrettAdamson <brettmadamson@gmail.com>
2024-07-30 19:51:39 -0500

    up to date with main

--- app/core/routes.py
+++ app/core/routes.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import render_template
 from app.forms import SongForm
 from app.core import bp
@@ -25,54 +24,10 @@ def song_search_form(id):
         return render_template(
             "song_form.html", title="Song Search", form=form, spotify_data=spotify_data
         )
-=======
-from flask import render_template, flash, session
-from app.forms import SongForm
-from app.core import bp
-from app.api import spotify_handler
-from app.auth import auth
-import requests
-import json
-
-# from app.api.routes import getdata
-
-
-# @bp.before_request
-# def check_tokens():
-# print("checking tokens")
-# if session["auth_code"]:
-#     auth_token = session["auth_code"]
-# elif session["client_code"]:
-#     credential_token = session["client_code"]
-# else:
-#     auth.get_client_credentials()
-# return
-
-# if True:
-#     authToken = auth.access_token()
-#     print(authToken)
-
-
-@bp.route("/")
-def index():
-    print("exited function")
-    user = {"username": "Brett"}
-    return render_template("index.html", title="Home", user=user)
-
-
-@bp.route("/search", methods=["GET", "POST"])
-def song_search():
-    form = SongForm()
-    if form.validate_on_submit():
-        print("valid")
-        flash("{} by {}".format(form.song.data, form.artist.data))
-        # return redirect("/index")
->>>>>>> origin/main
     else:
         print("invalid song")
 
     return render_template("song_form.html", title="Song Search", form=form)
-<<<<<<< HEAD
 
 
 def song_search_by_id(id):
@@ -127,5 +82,3 @@ def song_search(input_artist, input_song):
         print("invalid song")
 
     return "Invalid Inputs"
-=======
->>>>>>> origin/main

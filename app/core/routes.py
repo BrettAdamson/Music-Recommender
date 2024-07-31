b32dd4727a2bf89bcaf675428b645e7b949d7ad8
BrettAdamson <49770729+BrettAdamson@users.noreply.github.com>
2024-07-30 19:53:07 -0500

    Develop (#9)
    * Feature/html templates (#2)
    
    * Added config and base html and error html files
    
    * added forms.py and flask-wtforms
    
    * added basic submit form for song and artist
    
    * added dotenv and error popups for the song forms that are submitted while empty
    
    * Feature/spotify auth api (#4)
    
    * function that hits spotify access token endpoint works
    
    * endpoint for spotify access bearer token is up
    
    * added test blueprints for better project structure
    
    * refactoring where the api and url route code resides
    
    * refactored spotify token to the api blueprint
    
    * refactored the application structure to use flask blueprints properly
    
    * trying to figure out how to structure the files that call Spotify
    
    * renamed website blueprint to core blueprint
    
    * api blueprint works and core blueprint can now import spotify_handler
    
    * got the basic oauth2 authentication flow down with redirect
    
    * fixed wrong grant_type for authorization code OAuth flow
    
    * working on storing oauth token and being able to retrieve it for Spotify endpoints
    
    * added basic token decorater
    
    * working on token required decorated function
    
    * working on decorators for when requests are made when the access token is expired or empty
    
    * hitting the spotify api now, just need to figure out the best way to refresh the access token using date math
    
    * error decorator for 401's on client credentials works
    
    * refresh token works individually, but when put together with the decorator it doesnt work
    
    * found auth redirect problem, will need to redirect to a 'login with spotify' page, if the user is not authorized
    
    * Basic redirect button works for when the user does not have an auth code
    
    * Feature/spotify recommend form (#5)
    
    * working on getting search GET method from spotify working with Flask WTform
    
    * got the form to correctly query spotify
    
    * parsing through the api response for searching a track
    
    * search form shows song title and album image
    
    * getting the recommended tracks, will need to iterate over them to display them
    
    * iterating over recommended tracks, running into dictionary display issues
    
    * displaying all the images and song recommendations
    
    * Feature/css styling (#6)
    
    * adding tailwindcss
    
    * tailwindcss script complete, with all the html files in the template folder being targeted
    
    * added Flowbite for tailwindcss components
    
    * working on basic UI
    
    * working on flexbox for the images
    
    * basic css styling complete for displaying the recommended tracks
    
    * Feature/playing tracks (#7)
    
    * added the audio player for the current searched song
    
    * playing tracks feature works, when the preview url exists, needs some CSS changes still
    
    * added Plyr media player to customize the audio tag
    
    * Added: line clamp, card effects, plyr npm install, and colouring
    
    * added card to the main searched song
    
    * trying to get redirect to work, when clicking on a recommendation
    
    * added extra search track by id route, which is run when a user clicks on a recommendation to see more songs like that one
    
    * fixed form action for song search
    
    * updated href tag so that playing a recommended song doesnt reload the page
    
    * Feature/open track (#8)
    
    * trying to fix broken flexbox
    
    * fixed the css issue of the button div not filling the height of the parent div
    
    * refactored routes.py song search function
    
    * got the main track link to open in spotify, recommended tracks link does not work yet
    
    * added link to open in spotify
    
    * refactored and removed unused files and imports
    
    * up to date with main

--- app/core/routes.py
+++ app/core/routes.py
@@ -1,45 +1,84 @@
-from flask import render_template, flash, session
+from flask import render_template
 from app.forms import SongForm
 from app.core import bp
 from app.api import spotify_handler
-from app.auth import auth
-import requests
-import json
-
-# from app.api.routes import getdata
 
 
-# @bp.before_request
-# def check_tokens():
-# print("checking tokens")
-# if session["auth_code"]:
-#     auth_token = session["auth_code"]
-# elif session["client_code"]:
-#     credential_token = session["client_code"]
-# else:
-#     auth.get_client_credentials()
-# return
-
-# if True:
-#     authToken = auth.access_token()
-#     print(authToken)
+# @bp.route("/")
+# def index():
+#     print("exited function")
+#     return render_template("index.html", title="Home")
 
 
-@bp.route("/")
-def index():
-    print("exited function")
-    user = {"username": "Brett"}
-    return render_template("index.html", title="Home", user=user)
-
-
-@bp.route("/search", methods=["GET", "POST"])
-def song_search():
+@bp.route("/", methods=["GET", "POST"], defaults={"id": None})
+@bp.route("/<id>", methods=["GET", "POST"])
+def song_search_form(id):
     form = SongForm()
     if form.validate_on_submit():
-        print("valid")
-        flash("{} by {}".format(form.song.data, form.artist.data))
-        # return redirect("/index")
+        spotify_data = song_search(form.artist.data, form.song.data)
+        return render_template(
+            "song_form.html", title="Song Search", form=form, spotify_data=spotify_data
+        )
+    elif id is not None:
+        spotify_data = song_search_by_id(id)
+        return render_template(
+            "song_form.html", title="Song Search", form=form, spotify_data=spotify_data
+        )
     else:
         print("invalid song")
 
     return render_template("song_form.html", title="Song Search", form=form)
+
+
+def song_search_by_id(id):
+    if id is not None:
+        track = spotify_handler.get_track(id)
+        song_name = track["name"]
+        artist_name = track["artists"][0]["name"]
+        album_image = track["album"]["images"][0]["url"]
+        id = track["id"]
+        preview_url = track["preview_url"]
+        recommendations = spotify_handler.recommend_song_by_track(id)
+        recommendation_tracks = recommendations["tracks"]
+        spotify_url = track["external_urls"]["spotify"]
+        spotify_data = {
+            "song_name": song_name,
+            "artist_name": artist_name,
+            "album_image": album_image,
+            "recommendation_tracks": recommendation_tracks,
+            "preview_url": preview_url,
+            "spotify_url": spotify_url,
+        }
+        return spotify_data
+    else:
+        print("invalid song")
+
+    return "Invalid Inputs"
+
+
+def song_search(input_artist, input_song):
+    if input_artist and input_song:
+        track = spotify_handler.search_song(input_artist, input_song)
+        album_name = track["album"]["name"]
+        song_name = track["name"]
+        artist_name = track["artists"][0]["name"]
+        album_image = track["album"]["images"][0]["url"]
+        track_id = track["id"]
+        preview_url = track["preview_url"]
+        recommendations = spotify_handler.recommend_song_by_track(track_id)
+        recommendation_tracks = recommendations["tracks"]
+        spotify_url = track["external_urls"]["spotify"]
+        # return track
+        spotify_data = {
+            "song_name": song_name,
+            "artist_name": artist_name,
+            "album_image": album_image,
+            "recommendation_tracks": recommendation_tracks,
+            "preview_url": preview_url,
+            "spotify_url": spotify_url,
+        }
+        return spotify_data
+    else:
+        print("invalid song")
+
+    return "Invalid Inputs"

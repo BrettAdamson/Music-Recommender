fe1040e57c7285302b9151dea289592525d6d20e
BrettAdamson <brettmadamson@gmail.com>
2024-08-06 18:23:54 -0500

    added new css rules in tailwind config for colour scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./app/templates/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-shade':'#E3EAEB',
+        'light-accent':'#8797BD',
+        'main-colour':'#586292',
+        'dark-accent':'#7D86A5',
+        'dark-shades':'#12192B',
+      }
+    },
   },
   plugins: ["flowbite/plugin",],
 }
